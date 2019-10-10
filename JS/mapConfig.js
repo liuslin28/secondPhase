d813f14e98c95c078dce404f1d6974c1cc703b90
liuslin28 <liuslin28@163.com>
2019-10-10 19:31:17 +0800

    修改 地图配置

--- JS/mapConfig.js
+++ JS/mapConfig.js
@@ -1,14 +1,22 @@
 let source_layer_config = [
     {
         "source_id": "originalSource",
-        // "source_path": "./geojsonData/stopsPoint2.json",
         "source_title": "原始的公交线路",
         "source_type": "geojson"
     },
     {
         "source_id": "modifiedSource",
-        // "source_path": "./geojsonData/centerPolygon.json",
         "source_title": "修改的公交线路",
+        "source_type": "geojson"
+    },
+    {
+        "source_id": "originalLaneSource",
+        "source_title": "原始的公交专用道",
+        "source_type": "geojson"
+    },
+    {
+        "source_id": "modifiedLaneSource",
+        "source_title": "修改的公交专用道",
         "source_type": "geojson"
     }
 ];
@@ -45,6 +53,40 @@ let map_layer_config = [
         },
         "layer_paint": {
             "line-color": "#e95db5",
+            "line-opacity": 1,
+            "line-width": 2
+        },
+        "layer_filter": null
+    },
+    {
+        "layer_title": "原始的公交专用道",
+        "layer_id": "originalLaneLayer",
+        "source_id": "originalLaneSource",
+        "layer_type": "line",
+        "layer_layout": {
+            "line-join": "round",
+            "line-cap": "round",
+            "visibility": "visible"
+        },
+        "layer_paint": {
+            "line-color": "#d8d90e",
+            "line-opacity": 1,
+            "line-width": 2
+        },
+        "layer_filter": null
+    },
+    {
+        "layer_title": "修改的公交专用道",
+        "layer_id": "modifiedLaneLayer",
+        "source_id": "modifiedLaneSource",
+        "layer_type": "line",
+        "layer_layout": {
+            "line-join": "round",
+            "line-cap": "round",
+            "visibility": "visible"
+        },
+        "layer_paint": {
+            "line-color": "#e9701c",
             "line-opacity": 1,
             "line-width": 2
         },
